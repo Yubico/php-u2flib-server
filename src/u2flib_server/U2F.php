930dcaf19ef0b5b927f79bd268c4b2a35ec7f0b6
Klas Lindfors <klas@yubico.com>
2014-12-16 11:02:38 +0100

    add an override to the Error constructor
    making message and code mandatory

--- src/u2flib_server/U2F.php
+++ src/u2flib_server/U2F.php
@@ -338,6 +338,10 @@ class Registration {
 
 /** Error class, returned on errors */
 class Error extends \Exception {
+  /* Override constructor and make messange and code mandatory */
+  public function __construct($message, $code, Exception $previous = null) {
+    parent::__construct($message, $code, $previous);
+  }
 }
 
 ?>
